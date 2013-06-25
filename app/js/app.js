e52b4c1ecb1b526d5bdbb4b104b3a992057ec342
Yaroslav Sergeyev <noomorph@gmail.com>
2013-06-25 23:32:01 +0300

    Zero draft

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('todoApp', ['todoApp.filters', 'todoApp.services', 'todoApp.directives', 'todoApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/todo', {templateUrl: 'partials/todo.html', controller: 'Todo'});
-    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'About'});
+    $routeProvider.when('/todo', {templateUrl: 'partials/todo.html', controller: 'TodoCtrl'});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'});
     $routeProvider.otherwise({redirectTo: '/todo'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,30 @@
 /* Controllers */
 
 angular.module('todoApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('TodoCtrl', ['$scope', function($scope) {
+    $scope.projects = [
+        {
+            id: 1,
+            name: 'Complete the test task for Ruby Garage',
+            tasks: [
+                { text: 'Open this mock-up in Adobe Fireworks', done: true },
+                { text: 'Attentively check the file' },
+                { text: 'Write HTML & CSS' },
+                { text: 'Add JavaScript to implement adding / editing / removing for todo items and lists taking into account as more use cases as possible' }
+            ]
+        },
+        {
+            id: 2,
+            name: 'For Home',
+            tasks: [
+                { text: 'Buy a milk' },
+                { text: 'Call Mam' },
+                { text: 'Clean the room' },
+                { text: 'Repair the DVD Player' }
+            ]
+        }
+    ];
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('AboutCtrl', [function() {
 
   }]);
