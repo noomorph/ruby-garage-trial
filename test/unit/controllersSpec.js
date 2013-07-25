f700f94bbc1961eccf3eced5cc5b22e22dc9d09b
Yaroslav Sergeyev <noomorph@gmail.com>
2013-07-25 10:42:18 +0300

    Tests, bindings

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -26,60 +26,114 @@ describe('TodoController tests', function() {
         });
     });
 
+    it("after click on Add List we have one list more", function () {
+        var count = scope.lists.length;
+
+        scope.addTodoList();
+
+        expect(scope.lists.length).toBe(count + 1);
+        var list = scope.lists[count];
+        var empty = new TodoList(); 
+        expect(list.id).toBe(empty.id);
+        expect(list.name).toBe(empty.name);
+        expect(list.tasks.length).toBe(empty.tasks.length);
+    });
+
+    it("after click on remove list we have one list less", function () {
+        var count = scope.lists.length;
+        var list = scope.lists[0];
+
+        scope.removeList(0);
+        expect(scope.lists.length).toBe(count - 1);
+        expect(scope.lists).not.toContain(list);
+    });
+
     it("new list is empty", function () {
         var list = new TodoList();
+
         expect(list.id).toBeUndefined();
-        expect(list.name).toBe('');
+        expect(list.name).toBe('New list');
         expect(list.tasks.length).toBe(0);
     });
 
     it("add task with empty text does not work", function () {
         var list = new TodoList();
+
         list.add.do();
+
         expect(list.tasks.length).toBe(0);
     });
 
     it("after click on add task, new list is added and text is cleared", function () {
         var list = new TodoList();
+
         list.add.text = 'New task';
         list.add.do();
+
         expect(list.tasks.length).toBe(1);
         expect(list.tasks[0].text).toBe('New task');
         expect(list.add.text).toBe('');
     });
 
+    var createListWithTasks = function (count) {
+        var i, tasks = [];
+        for (i = 0; i < count; i++) {
+            tasks.push({ text: 'Task ' + i });
+        }
+        return new TodoList({ tasks: tasks });
+    };
+
     it("after click on remove task, it is removed", function () {
-        var list = new TodoList();
-        list.tasks.push({ text: 'Sample task' });
+        var list = createListWithTasks(1);
+
         list.removeAt(0);
+
         expect(list.tasks.length).toBe(0);
     });
 
     it("should mark item as editable after button click", function () {
-        var list = new TodoList();
-        list.tasks.push({ text: 'Sample task'});
+        var list = createListWithTasks(1);
+        var task = list.tasks[0];
+
         list.edit(0);
-        expect(list.tasks[0].editable).toBe(true);
+
+        expect(task.editable).toBe(true);
+        expect(task.lastValue).toBe(task.text);
     });
 
-    it("should unmark editable item after edit is cancelled", function () {
-        var list = new TodoList({ 
-            tasks: [{ text: 'A task' }]
-        });
+    it("should unmark editable item and revert text after edit is cancelled", function () {
+        var list = createListWithTasks(1);
+        var task = list.tasks[0];
+        var text = task.text;
+
         list.edit(0);
+        task.text = 'Another text';
         list.cancelEdit();
-        expect(list.tasks[0].editable).toBe(false);
+
+        expect(task.editable).toBe(false);
+        expect(task.lastValue).toBeUndefined();
+        expect(task.text).toBe(text);
+    });
+
+    it("should unmark editable item and apply text after edit is confirmed", function () {
+        var list = createListWithTasks(1);
+        var task = list.tasks[0];
+
+        list.edit(0);
+        task.text = 'Another text';
+        list.confirmEdit(0);
+
+        expect(task.editable).toBe(false);
+        expect(task.lastValue).toBeUndefined();
+        expect(task.text).toBe('Another text');
     });
 
     it("should reset current editable item after you start editing the other", function () {
-        var list = new TodoList({
-            tasks: [
-                { text: 'Task 1' },
-                { text: 'Task 2' }
-            ]
-        });
+        var list = createListWithTasks(2);
+
         list.edit(0);
         list.edit(1);
+
         expect(list.tasks[0].editable).toBe(false);
         expect(list.tasks[1].editable).toBe(true);
     });
